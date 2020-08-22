c4faff8833eff78e579ef59c352757b861d53007
chmelan <chmelan@comcast.net>
2020-08-22 09:36:20 -0400

    fixed genre model export bug

--- models/genre.js
+++ models/genre.js
@@ -2,12 +2,14 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var GenreInstanceSchema = new Schema({
-  name: { type: String, minlength: 3, maxlength: 100 },
+var GenreSchema = new Schema({
+  name: { type: String, required: true, min: 3, max: 100 },
 });
 
-GenreInstanceSchema.virtual("url").get(() => {
-  return "/catalog/genreinstance/" + this._id;
+// Virtual for this genre instance URL.
+GenreSchema.virtual("url").get(function () {
+  return "/catalog/genre/" + this._id;
 });
 
-module.exports - mongoose.model("GenreInstance", GenreInstanceSchema);
+// Export model.
+module.exports = mongoose.model("Genre", GenreSchema);
